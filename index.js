c8d84dc11a2690ee935b31c1a43ad21fa6f0239f
bobbyhadz <opmftw@gmail.com>
2024-03-01 18:26:42 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// EXAMPLE 1 - Get the First Word of a String in JavaScript
+
+const str = 'Hello world';
+
+const first = str.split(' ')[0];
+console.log(first); // 👉️ Hello
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 2 - Get the First Word of a String using Array.shift()
+
+// const str = 'Hello world';
+
+// const first = str.split(' ').shift();
+// console.log(first); // 👉️ Hello
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 3 - Get the First Word of a String using String.slice()
+
+// const str = 'Hello world';
+
+// const firstWord = str.slice(0, str.indexOf(' '));
+
+// console.dir(firstWord); // 👉️ 'Hello'
